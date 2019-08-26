58e5f4f1411b255de218a26edd74b11ad89a6aff
YeonSungMin <meisensei0312@hotmail.com>
2019-08-26 15:24:42 +0900

    Add SoundLibrary Create Class in Sound.h

--- Thirteen/Sound.h
+++ Thirteen/Sound.h
@@ -15,59 +15,81 @@ bool isSuccess = soundsManager.Initialize();
 // 第2引数は音声ファイルを識別するための任意の文字列をキーとして指定する。
 // この後の操作関数の呼び出し時には、ここで設定したキーを指定して音声を識別する。
 
-const TCHAR* filePath = _T("音楽ファイル");
-bool isSuccess = soundsManager.AddFile(filePath, _T("bgm"));
 
+class OpenMusic {
 
+public:
+	const TCHAR* filePath = _T("音楽ファイル");
+	bool isSuccess = soundsManager.AddFile(filePath, _T("bgm"));
 
-// 頭から再生
-// 一時停止中の音声に対して当関数を実行した場合も頭からの再生となる。
-// 第2引数にtrueを渡すとループ再生になる。
-bool isSuccess = soundsManager.Start(_T("bgm"), true);
-
-// 一時停止
-bool isSuccess = soundsManager.Pause(_T("bgm"));
-
-// 一時停止中の音声を続きから再生
-bool isSuccess = soundsManager.Resume(_T("bgm"));
-
-// 再生停止
-bool isSuccess = soundsManager.Stop(_T("bgm"));
-
-// 再生状態を取得
-SoundLib::PlayingStatus status = soundsManager.GetStatus(_T("bgm"));
-
-
-/// <summary>
-/// 再生状況を示すステータス
-/// </summary>
-enum PlayingStatus {
-	/// <summary>停止中</summary>
-	Stopped,
-	/// <summary>再生中</summary>
-	Playing,
-	/// <summary>一時停止中</summary>
-	Pausing
 };
 
+class PlayMusic {
+
+public:
+
+	bool isSuccess()
+	{
+		// 頭から再生
+		// 一時停止中の音声に対して当関数を実行した場合も頭からの再生となる。
+		// 第2引数にtrueを渡すとループ再生になる。
+		soundsManager.Start(_T("bgm"), true);
+
+		// 一時停止
+		soundsManager.Pause(_T("bgm"));
+
+		// 一時停止中の音声を続きから再生
+		soundsManager.Resume(_T("bgm"));
+
+		// 再生停止
+		soundsManager.Stop(_T("bgm"));
+
+	}
+
+	// 再生状態を取得
+	SoundLib::PlayingStatus status = soundsManager.GetStatus(_T("bgm"));
 
 
-//　ボリューム変更
-// 0(無音)～100(原音量)の間で設定可能
-soundsManager.SetVolume(_T("bgm"), 50);
+	/// <summary>
+	/// 再生状況を示すステータス
+	/// </summary>
+	enum PlayingStatus {
+		/// <summary>停止中</summary>
+		Stopped,
+		/// <summary>再生中</summary>
+		Playing,
+		/// <summary>一時停止中</summary>
+		Pausing
+	};
 
-// ボリューム取得
-uint8_t volume = soundsManager.GetVolume(_T("bgm"));
+};
 
+class VolumeMix
+{
 
+public:
 
-// 再生速度・ピッチ変更
-bool isSuccess = soundsManager.SetFrequencyRatio(_T("bgm"), 1.0f);
+	//　ボリューム変更
+	// 0(無音)～100(原音量)の間で設定可能
+	bool isSuccess = soundsManager.SetVolume(_T("bgm"), 50);
 
-// 再生速度・ピッチ取得
-float ratio = soundsManager.GetFrequencyRatio(_T("bgm"));
+	// ボリューム取得
+	uint8_t volume = soundsManager.GetVolume(_T("bgm"));
 
+};
 
+class MusicPitch 
+{
+
+public:
+
+	// 再生速度・ピッチ変更
+	bool isSuccess = soundsManager.SetFrequencyRatio(_T("bgm"), 1.0f);
+
+	// 再生速度・ピッチ取得
+	float ratio = soundsManager.GetFrequencyRatio(_T("bgm"));
+
+};
 
 class Foo {
 public:
