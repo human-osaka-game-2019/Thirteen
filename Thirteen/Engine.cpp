b1f7b5153acf1e23ce19fbc33947bf627cdc28cb
YeonSungMin <meisensei0312@hotmail.com>
2019-08-26 14:40:26 +0900

    delete old files

---
+++ Thirteen/Engine.cpp
@@ -0,0 +1,23 @@
+#include "Window.h"
+#include <Windows.h>
+#include "Engine.h"
+
+//　エンジンの初期化
+BOOL InitEngine(int width, int height, HINSTANCE hInst)
+{
+	HWND hWnd;
+	InitWnd window;
+
+	if (window.m_MakeWnd(hInst, width, height) == false)
+	{
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
+//　メモリを開放する
+VOID EndEngine()
+{
+
+}
