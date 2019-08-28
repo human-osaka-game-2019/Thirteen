976246cee7726148c2d626abcb2ea41068a905db
Kazuma_Okamoto <rikazuma.culb@gmail.com>
2019-08-28 10:05:58 +0900

    自機の移動、自機が弾を発射する関数の作成 #5

--- Thirteen/Player.h
+++ Thirteen/Player.h
@@ -1,1 +1,28 @@
-#pragma once
+#ifndef PLAYER_H_
+#define PLAYER_H_
+
+class Player
+{
+public:
+	float x, y;
+	float m_speed;
+	float m_moving_distance;
+	int row, col;
+	int m_frame_count;
+	int m_key_check;
+	bool key_push_flag;
+	void m_PlayerMove(Player* player, int MapChipList[][28]);
+};
+
+class Bullet
+{
+public:
+	float x, y;
+	float m_speed;
+	int m_frame_count;
+	int m_key_check;
+	bool key_push_flag;
+	void m_FireBullet(Bullet* bullet);
+};
+
+#endif
